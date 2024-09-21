0f7499eeddd1bda538873597171485bc46ad7d9d
Martin "MartinGames" Petr <martinpetrnp@gmail.com>
2024-09-21 15:54:33 +0200

    Add support for bundling for windows

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,35 @@ import path from "path";
 import { cpSync, existsSync, rmdirSync, writeFileSync } from "fs";
 import { $, readableStreamToText } from "bun";
 
-export default async function bundle(root_path: string) {
-  console.log("Bundling %s...", root_path);
+const platforms = {
+  linux: {
+    bun_target: "bun-linux-x64",
+    temp: "/tmp/",
+    executable: "AppRun",
+    output: "app",
+  },
+  windows: {
+    bun_target: "bun-windows-x64",
+    temp: "C:\\Windows\\Temp\\",
+    executable: "AppRun.exe",
+    output: "app.exe",
+  },
+};
+
+export default async function bundle(
+  root_path: string,
+  platform_id?: keyof typeof platforms,
+) {
+  const platform = platform_id
+    ? platforms[platform_id]
+    : platforms[process.platform == "win32" ? "windows" : "linux"];
+
+  if (!platform) {
+    console.error("Unknown platform.");
+    process.exit(1);
+  }
+
+  console.log("Bundling %s for %s...", root_path, platform_id);
 
   if (existsSync(path.join(root_path, "bundle")))
     rmdirSync(path.join(root_path, "bundle"), {
@@ -32,7 +59,8 @@ export default async function bundle(root_path: string) {
   writeFileSync(
     path.join(root_path, "bundle/config.json"),
     JSON.stringify({
-      path: "/tmp/" + Math.random().toString(36).substring(7),
+      path: platform.temp + Math.random().toString(36).substring(7),
+      entrypoint: platform.executable,
     }),
   );
 
@@ -53,7 +81,16 @@ export default async function bundle(root_path: string) {
 
   await new Promise((r) =>
     Bun.spawn({
-      cmd: ["bun", "build", "--compile", "--outfile", "app", "entrypoint.ts"],
+      cmd: [
+        "bun",
+        "build",
+        "--compile",
+        "--outfile",
+        platform.output,
+        "entrypoint.ts",
+        "--target",
+        platform.bun_target,
+      ],
       cwd: path.join(root_path, "bundle"),
       stdio: ["inherit", "inherit", "inherit"],
       onExit: r,
